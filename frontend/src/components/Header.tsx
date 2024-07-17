d5371efe30ef935e6438bd59eca7189ce70c0d11
d-pamneja <dpamneja@gmail.com>
2024-07-17 19:57:16 +0530

    Logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
+import Logo from './shared/Logo';
 
 
 
 const Header = () => {
     return <AppBar sx={{bgcolor : "#9932CC", position : "static"}}>
-        <Toolbar sx={{display : "flex"}}>
-
+        <Toolbar sx={{display : "flex", justifyContent : 'center'}}>
+            <Logo />
         </Toolbar>
     </AppBar>
 };
--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -1,10 +1,46 @@
+import { Typography } from '@mui/material';
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 
 
 
 const Logo = () => {
-    return <div>Logo</div>
+    return (
+    <div
+        style={{
+            display : 'flex', 
+            marginTop : '10px',
+            justifyContent : 'center',
+            alignItems : 'center',
+            gap : '8px'
+        }}
+    >
+        <Link to='/'>
+            <img 
+                src = "chitra_logo.png" 
+                alt = "Google Gemini" 
+                width={'250px'} 
+                height={'100px'}
+            />
+        </Link>
+        {/* <Typography sx={{
+                    display : {
+                        md : "block",
+                        sm : "none",
+                        xs : "none"
+                    },
+                    marginRight : "auto",
+                    fontWeight : "800",
+                    textShadow : "2px 2px 20px #000"
+                }}>
+                    <span style={{
+                        fontSize : "10px",
+                        marginBottom : "auto"
+                    }}>MERN Application + Google Gemini</span>
+        </Typography> */}
+    </div>
+    )
 };
 
 
