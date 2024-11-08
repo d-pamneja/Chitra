48a8797fd1309d548e04e811f41d58d28d2dfce7
d-pamneja <dpamneja@gmail.com>
2024-11-09 00:07:45 +0530

    gemini setup

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -32,7 +32,9 @@ const Header = () => {
     };
 
     return(
-        <nav className='sticky top-0 z-50 py-3 background-blur-lg border-b border-neutral-700/80'>
+        <nav 
+            className='sticky top-0 z-50 py-3 backdrop-blur-md bg-[#0c0660] bg-opacity-70 border-b'
+        >
             <div className='container px-4 mx-auto relative text-sm'>
                 <div className="flex justify-between items-center">
                     <div className="flex items-center flex-shrink-0">
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,9 +1,11 @@
-import { HeroHighlightInstance } from '../components/ui/highlight-text';
+import { GoogleGeminiEffectInstance } from '../components/ui/gemini-effect';
+// import { HeroHighlightInstance } from '../components/ui/highlight-text';
 
 const Home = () => {
     return <div>
         {/* <VortexInstance/> */}
-        <HeroHighlightInstance/>
+        <GoogleGeminiEffectInstance/>
+        {/* <HeroHighlightInstance/> */}
         <div className="h-screen">
             BKC
         </div>
