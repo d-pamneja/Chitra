328d521b73712fd085ec3af91f511827beb920dd
d-pamneja <dpamneja@gmail.com>
2024-11-12 12:56:09 +0530

    Caoursel Responsiveness

--- frontend/src/components/ui/scorsese-carousel.tsx
+++ frontend/src/components/ui/scorsese-carousel.tsx
@@ -32,7 +32,7 @@ export function ScorseseCarousel() {
             >
                 <div
                     id="slider"
-                    className="animate-[autoRunCarousel_20s_linear_infinite] absolute top-10 left-[calc(50%-100px)] w-[200px] h-[250px] z-20"
+                    className="animate-[autoRunCarousel_20s_linear_infinite] absolute top-10 left-[calc(50%-100px)] xl:w-[200px] lg:w-[188px] md:w-[175px] w-[150px] xl:h-[250px] lg:h-[234px] md:h-[219px] h-[188px] z-20"
                     style={{
                         transformStyle:"preserve-3d",
                         transform : "perspective(1000px)"
@@ -53,13 +53,13 @@ export function ScorseseCarousel() {
 
                 <div
                     id="content"
-                    className="absolute flex flex-wrap justify-between items-center bottom-[90px] left-[700px] min-w-[1400px] h-max pb-[100px]"
+                    className="absolute flex flex-wrap justify-between items-center bottom-[90px] xl:left-[700px] lg:left-[500px] md:left-[375px] left-[250px] xl:min-w-[1400px] lg:min-w-[1200px] md:min-w-[1000px] min-w-[600px] h-max pb-[100px]"
                     style={{
                         transform : "translateX(-50%)"
                     }}
                 >
                     <h1 
-                        className="relative font-icaRubrik text-[4em] ml-[150px] leading-12 z-50"
+                        className="relative font-icaRubrik xl:text-[4em] lg:text-[3em] text-[2em] xl:ml-[150px] lg:ml-[220px] md:ml-[250px] ml-[100px] leading-12 z-50"
                         data-content="Your Next Favorite Flick, Just a Chat Away!"
                         style={{
                             color: "white"
